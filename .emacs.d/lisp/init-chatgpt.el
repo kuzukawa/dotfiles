2dbdb2687be62e964f36f6f62aa952b9c911001e
Keisuke Kuzukawa <kuzukawa@gmail.com>
2023-11-30 20:02:13 +0900

    fix: remove ...

--- .emacs.d/lisp/init-chatgpt.el
+++ .emacs.d/lisp/init-chatgpt.el
@@ -4,15 +4,6 @@
 ;; chatgpt configuration
 
 ;;; Code:
-;(setq python-interpreter "python")
-;(setenv "OPENAI_API_KEY" "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")
-
-;(use-package chatgpt
-;  :straight (:host github :repo "joshcho/ChatGPT.el" :files ("dist" "*.el"))
-;  :init
-;  (require 'python)
-;  (setq chatgpt-repo-path "~/.emacs.d/straight/repos/ChatGPT.el/")
-;  :bind ("C-c q" . chatgpt-query))
 (use-package openai :straight (:host github :repo "emacs-openai/openai"))
 (use-package chatgpt :straight (:host github :repo "emacs-openai/chatgpt"))
 (use-package codegpt :straight (:host github :repo "emacs-openai/codegpt"))
